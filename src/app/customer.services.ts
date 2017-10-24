89087c08faaaacf29225c5bd5f0273a87d76825e
abdullah-nsr <abdullah.nasri@outlook.sa>
2017-10-25 00:51:32 +0200

    added

---
+++ src/app/customer.services.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+import { Customer } from './customer';
+import { CUSTOMERS } from './mock-customers';
+
+@Injectable()
+export class CustomerService {
+    getCustomers(): Promise<Customer[]> {
+        return Promise.resolve(CUSTOMERS);
+      }
+}
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+
+//main entry point
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {AppModule} from './app/app.module';
+
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+

909f34e81a59f355e3cf1f936759d19d9549d894
Abdullah Nasri <abdullah.nasri@plista.local>
2017-11-01 17:31:31 +0100

    added

---
+++ app/customer-detail.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core'
+import { Customer } from './customer'
+
+
+@Component({
+    selector: 'customer-detail',
+    template: `<h1>{{customer.a}}</h1> 
+    <div *ngIf="customer">
+    <h2>{{customer.PlistaProduct}} details!</h2>
+
+     <div><label>a: </label>{{customer.a}}</div>
+       <div>
+        <label>PlistaProduct: </label>
+        <input [(ngModel)]="customer.PlistaProduct" placeholder="PlistaProduct"/>
+       </div>
+    </div>
+ `
+})
+
+export class CustomerDetailComponent  {
+ 
+  
+  @Input() customer: Customer;
+
+}
